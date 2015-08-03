5b4e9f58a2d713d38b2d147be90020f52125db36
lapacino <lapacino@lapacinos-Mac-Pro.local>
2015-08-03 19:46:26 +0100

    weatherApp

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var weatherApp: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func weatherButtonPressed(sender: UIButton) {
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityNameTextField.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                var weather = ""
+                
+                if error == nil {
+                    
+                    let urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    let urlcontentArray = urlContent!.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlcontentArray.count > 0 {
+                        var weatherArray = urlcontentArray[1].componentsSeparatedByString("</span>")
+                       var weather1 = weatherArray[0] as! String
+                       weather = weather1.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                    }
+                    else{
+                        urlError = true
+                    }
+                    
+                }
+                else{
+                    urlError = true
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    if urlError == true {
+                        self.showUrlError()
+                    }
+                    else{
+                        self.factLabel.text = weather
+                        self.weatherApp.text = "" + self.cityNameTextField.text + " Weather"
+                    }
+                }
+                
+                
+            })
+            task.resume()
+            
+          
+            
+            
+        }
+        else{
+           
+            showUrlError()
+        }
+   
+            
+       
+    
+    }
+    
+    func showUrlError() {
+        
+        factLabel.text = "loading of " + cityNameTextField.text + " failed. please try again"
+    }
 
 }
 
